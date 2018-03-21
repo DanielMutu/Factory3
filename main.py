93c1119b932d3ba9a91ed487f50e7e8c099712a8
Xubuntu VM Lynx <danielmutu96@hotmail.it>
2018-03-21 17:43:31 +0100

    funzionalità kivy 01

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from functools import partial
 
 
 class LoginScreen(GridLayout):
@@ -20,15 +24,47 @@ class LoginScreen(GridLayout):
 
          self.add_widget(Button(text='BUTTON'))
 
+class Test(App):
+    def create_clock(self, widget, touch, *args):
+        callback = partial(self.menu, touch)
+        Clock.schedule_once(callback, 2)
+        touch.ud['event'] = callback
+
+    def delete_clock(self, widget, touch, *args):
+        Clock.unschedule(touch.ud['event'])
+
+    def menu(self, touch, *args):
+        menu = BoxLayout(
+                size_hint=(None, None),
+                orientation='vertical',
+                center=touch.pos)
+        menu.add_widget(Button(text='a'))
+        menu.add_widget(Button(text='b'))
+        close = Button(text='close')
+        close.bind(on_release=partial(self.close_menu, menu))
+        menu.add_widget(close)
+        self.root.add_widget(menu)
+
+    def close_menu(self, widget, *args):
+        self.root.remove_widget(widget)
+
+    def build(self):
+        self.root = FloatLayout()
+        self.root.bind(
+            on_touch_down=self.create_clock,
+            on_touch_up=self.delete_clock)
+
+
+
 
 class MyApp(App):
 
     def build(self):
-        return LoginScreen()
+        return Test()
 
 
 if __name__ == '__main__':
-    MyApp().run() #entry point
+    Test().run() #entry point
 
 
 
